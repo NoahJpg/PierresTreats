9cd4e5e1aabb943f24588d16082d5f189f6b69fb
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-03-24 08:29:32 -0700

    create initial file structure

---
+++ PierresTreats/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PierresTreats.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace PierresTreats
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      builder.Services.AddDbContext<PierresTreatsContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
+
+      // New code below!!
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<PierresTreatsContext>()
+                .AddDefaultTokenProviders();
+
+      WebApplication app = builder.Build();
+
+      // app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      // New code below!
+      app.UseAuthentication(); 
+      app.UseAuthorization();
+
+      app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
+      app.Run();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# _Pierres Treats_
+
+#### By Noah Atkinson
+
+#### _A MVC app that allows users to keep track of treats featuring authentication and authorization._
+
+## Technologies Used
+
+* _C#_
+* _.NET_
+* _HTML_
+* _MVC_
+* _SQL Workbench_
+* _Entity Framework_
+* _CSS_
+
+
